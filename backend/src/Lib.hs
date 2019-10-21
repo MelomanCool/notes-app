6056bc908258bcf0b0eb6f49d8272d15cfb95a78
MelomanCool <melomancool@gmail.com>
2019-10-21 23:15:45 +0400

    err500, eitherReadFile

--- backend/src/Lib.hs
+++ backend/src/Lib.hs
@@ -9,15 +9,17 @@ module Lib
     , Note
     ) where
 
+import           Control.Exception (IOException, catch)
 import           Control.Monad.IO.Class (liftIO)
-import           Data.Aeson (ToJSON, FromJSON, decode)
+import           Data.Aeson (ToJSON, FromJSON, eitherDecode)
 import qualified Data.ByteString.Lazy as B
+import           Data.ByteString.Lazy.UTF8 as BLU
 import           Data.Maybe (fromMaybe)
 import           GHC.Generics (Generic)
 import           Network.Wai (Application)
 import           Network.Wai.Handler.Warp (run)
 import           Network.Wai.Middleware.Cors (simpleCors)
-import           Servant (serve, Proxy(..), Server, JSON, Get, (:>), Handler, throwError, err404)
+import           Servant (serve, Proxy(Proxy), Server, JSON, Get, (:>), Handler, throwError, err500, errBody)
 
 
 data Tag = Tag { id :: Int
@@ -38,15 +40,21 @@ instance ToJSON Note
 instance FromJSON Note
 
 
-loadNotes :: IO (Maybe [Note])
-loadNotes = fmap decode (B.readFile "notes.json")
+eitherReadFile :: FilePath -> IO (Either String ByteString)
+eitherReadFile p =
+  (Right <$> B.readFile p) `catch`
+  ((\e -> pure (Left $ show e)) :: IOException -> IO (Either String ByteString))
+
+
+loadNotes :: IO (Either String [Note])
+loadNotes = fmap (>>= eitherDecode) (eitherReadFile "notes.json")
 
 notesHandler :: Handler [Note]
 notesHandler = do
   mNotes <- liftIO $ loadNotes
   case mNotes of
-    Just notes -> return notes
-    Nothing    -> throwError err404
+    Right notes -> return notes
+    Left error  -> throwError err500 { errBody = BLU.fromString error }
 
 
 type NoteAPI = "notes" :> Get '[JSON] [Note]
